e68537d86009d66c3ea9c2904ea61ec1d92fe2fd
hoangNH <hoangcdnbk@gmail.com>
2024-12-18 21:00:13 +0700

    add java jsp

---
+++ server_time/src/main/java/com/codegym/server_time/ServerTimeServlet.java
@@ -0,0 +1,22 @@
+package com.codegym.server_time;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", urlPatterns = "/index")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("");
+        Date today = new Date();
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("");
+    }
+}
